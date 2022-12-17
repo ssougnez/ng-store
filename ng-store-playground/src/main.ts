6630a767181dd253b59ab8d020263e982ee5d89c
ssougnez <sebastien.sougnez@areaprog.com>
2022-12-17 22:58:08 +0100

    (updated): Documentation

--- ng-store-playground/src/main.ts
+++ ng-store-playground/src/main.ts
@@ -9,7 +9,10 @@ import { initial as initialSelectExample } from './examples/02_Select/state/app.
 import { AppComponent as IndexesComponent } from './examples/03_Indexes/app.component';
 import { initial as initialIndexesExample } from './examples/03_Indexes/state/app.state';
 
-const component = IndexesComponent;
+import { AppComponent as UpdateComponent } from './examples/04_Update/app.component';
+import { initial as initialUpdateExample } from './examples/04_Update/state/app.state';
+
+const component = UpdateComponent;
 
 const configurations = new Map();
 
@@ -33,6 +36,14 @@ configurations.set(IndexesComponent, [{
   provide: NG_STORE_CONFIG,
   useValue: {
     initialValue: initialIndexesExample,
+    httpClientType: null
+  } as StoreConfiguration
+}]);
+
+configurations.set(UpdateComponent, [{
+  provide: NG_STORE_CONFIG,
+  useValue: {
+    initialValue: initialUpdateExample,
     httpClientType: null
   } as StoreConfiguration
 }]);
