f4c32527b4e013fe2ae1f8b84a4bcc8c7fc8eb87
ssougnez <sebastien.sougnez@areaprog.com>
2022-12-17 23:09:15 +0100

    (updated): Documentation

--- ng-store-playground/src/main.ts
+++ ng-store-playground/src/main.ts
@@ -9,10 +9,31 @@ import { initial as initialSelectExample } from './examples/02_Select/state/app.
 import { AppComponent as IndexesComponent } from './examples/03_Indexes/app.component';
 import { initial as initialIndexesExample } from './examples/03_Indexes/state/app.state';
 
-import { AppComponent as UpdateComponent } from './examples/04_Update/app.component';
-import { initial as initialUpdateExample } from './examples/04_Update/state/app.state';
+import { AppComponent as UpsertComponent } from './examples/04_Upsert/app.component';
+import { initial as initialUpsertExample } from './examples/04_Upsert/state/app.state';
 
-const component = UpdateComponent;
+import { AppComponent as RemoveComponent } from './examples/05_Remove/app.component';
+import { initial as initialRemoveExample } from './examples/05_Remove/state/app.state';
+
+import { AppComponent as LoadComponent } from './examples/06_Load/app.component';
+import { initial as initialLoadExample } from './examples/06_Load/state/app.state';
+
+import { AppComponent as PostComponent } from './examples/07_Post/app.component';
+import { initial as initialPostExample } from './examples/07_Post/state/app.state';
+
+import { AppComponent as PutComponent } from './examples/08_Put/app.component';
+import { initial as initialPutExample } from './examples/08_Put/state/app.state';
+
+import { AppComponent as DeleteComponent } from './examples/09_Delete/app.component';
+import { initial as initialDeleteExample } from './examples/09_Delete/state/app.state';
+
+import { AppComponent as ContainerComponent } from './examples/10_Container/app.component';
+import { initial as initialContainerExample } from './examples/10_Container/state/app.state';
+
+import { AppComponent as ComponentComponent } from './examples/11_Component/app.component';
+import { initial as initialComponentExample } from './examples/11_Component/state/app.state';
+
+const component = RemoveComponent;
 
 const configurations = new Map();
 
@@ -40,10 +61,66 @@ configurations.set(IndexesComponent, [{
   } as StoreConfiguration
 }]);
 
-configurations.set(UpdateComponent, [{
+configurations.set(UpsertComponent, [{
   provide: NG_STORE_CONFIG,
   useValue: {
-    initialValue: initialUpdateExample,
+    initialValue: initialUpsertExample,
+    httpClientType: null
+  } as StoreConfiguration
+}]);
+
+configurations.set(RemoveComponent, [{
+  provide: NG_STORE_CONFIG,
+  useValue: {
+    initialValue: initialRemoveExample,
+    httpClientType: null
+  } as StoreConfiguration
+}]);
+
+configurations.set(LoadComponent, [{
+  provide: NG_STORE_CONFIG,
+  useValue: {
+    initialValue: initialLoadExample,
+    httpClientType: null
+  } as StoreConfiguration
+}]);
+
+configurations.set(PostComponent, [{
+  provide: NG_STORE_CONFIG,
+  useValue: {
+    initialValue: initialPostExample,
+    httpClientType: null
+  } as StoreConfiguration
+}]);
+
+configurations.set(PutComponent, [{
+  provide: NG_STORE_CONFIG,
+  useValue: {
+    initialValue: initialPutExample,
+    httpClientType: null
+  } as StoreConfiguration
+}]);
+
+configurations.set(DeleteComponent, [{
+  provide: NG_STORE_CONFIG,
+  useValue: {
+    initialValue: initialDeleteExample,
+    httpClientType: null
+  } as StoreConfiguration
+}]);
+
+configurations.set(ContainerComponent, [{
+  provide: NG_STORE_CONFIG,
+  useValue: {
+    initialValue: initialContainerExample,
+    httpClientType: null
+  } as StoreConfiguration
+}]);
+
+configurations.set(ComponentComponent, [{
+  provide: NG_STORE_CONFIG,
+  useValue: {
+    initialValue: initialComponentExample,
     httpClientType: null
   } as StoreConfiguration
 }]);
